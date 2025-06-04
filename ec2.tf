3bb574eaecf03933414c81335187d3f30df390d5
Jochen Benzenhöfer <jochen.benzenhoefer@it-tem.de>
2025-06-04 17:17:31 +0200

    finished infrastructure

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,26 @@
+# data "aws_ami" "Amazon_Linux_2023" {
+
+#   filter {
+#     name   = "name"
+#     values = ["Amazon Linux 2023 AMI"]
+#   }
+# }
+
+# import {
+#   to = data.aws_key_pair.keypair
+#   id = "vockey"
+# }
+
+data "aws_key_pair" "keypair" {
+  key_name = "vockey"
+}
+
 # Create a EC2 Instance with Wordpress
+resource "aws_instance" "Wordpress_Server" {
+  ami                         = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type               = "t3.micro"
+  security_groups             = [aws_security_group.SSH_Security_Group.id]
+  subnet_id                   = aws_subnet.Public_Subnet.id
+  associate_public_ip_address = true
+  key_name                    = data.aws_key_pair.keypair.key_name
+}
--- sg.tf
+++ sg.tf
@@ -1,15 +1,15 @@
 # Create a Security Group
 resource "aws_security_group" "SSH_Security_Group" {
-    description = "Allows SSH Access for Me"
-    name = "SSH_Security_Group"
-    vpc_id = aws_vpc.Wordpress_VPC.id
+  description = "Allows SSH Access for Me"
+  name        = "SSH_Security_Group"
+  vpc_id      = aws_vpc.Wordpress_VPC.id
 }
 
 # Allow SSH ingress
 resource "aws_vpc_security_group_ingress_rule" "SSH_Rule" {
   security_group_id = aws_security_group.SSH_Security_Group.id
-  ip_protocol = "tcp"
-  from_port = 22
-  to_port = 22
-  cidr_ipv4 = "${data.http.myip.response_body}/32"
+  ip_protocol       = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_ipv4         = "${data.http.myip.response_body}/32"
 }
