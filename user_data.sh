8bb9a6f54146ca4ef48e5f80de063e9036f7e86b
Jochen Benzenhöfer <jochen.benzenhoefer@it-tem.de>
2025-07-02 14:36:48 +0200

    Fix RDS integration in user-script

--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum update -y
-yum install -y php php-mysqlnd unzip httpd
+yum install -y php php-mysqlnd mariadb105-server unzip httpd
 systemctl start httpd
 systemctl enable httpd
 cd /var/www/html
@@ -9,14 +9,14 @@ unzip latest.zip
 cp -r wordpress/* .
 rm -rf wordpress latest.zip
 chown -R apache:apache /var/www/html
-sudo mysql -p -u root
+sudo mysql -p${db_password} -u root -h ${db_address} wordpress
 
-CREATE USER 'wpuser'@'${db_address}' IDENTIFIED BY '${db_password}';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'${db_address}';
+CREATE USER 'wpuser'@'${db_address}:${db_port}' IDENTIFIED BY '${db_password}';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'${db_address}:${db_port}';
 FLUSH PRIVILEGES;
 Exit;
 cp wp-config-sample.php wp-config.php
 sed -i "/DB_NAME/c\define('DB_NAME', 'wordpress');'" wp-config.php
 sed -i "/DB_USER/c\define('DB_USER', 'wpuser');" wp-config.php
 sed -i "/DB_PASSWORD/c\define('DB_PASSWORD', '${db_password}');" wp-config.php
-sed -i "/DB_HOST/c\define('DB_HOST', '${db_address}');" wp-config.php
+sed -i "/DB_HOST/c\define('DB_HOST', '${db_address}:${db_port}');" wp-config.php
