69bd520016060e66727aff093dd63f52c8b4ece4
Jochen Benzenhöfer <jochen.benzenhoefer@it-tem.de>
2025-07-03 10:32:06 +0200

    fix user_data Script

--- user_data.sh
+++ user_data.sh
@@ -9,12 +9,18 @@ unzip latest.zip
 cp -r wordpress/* .
 rm -rf wordpress latest.zip
 chown -R apache:apache /var/www/html
-sudo mysql -p${db_password} -u root -h ${db_address} wordpress
 
-CREATE USER 'wpuser'@'${db_address}:${db_port}' IDENTIFIED BY '${db_password}';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'${db_address}:${db_port}';
-FLUSH PRIVILEGES;
-Exit;
+touch temp.sql
+
+echo "CREATE USER IF NOT EXISTS 'wpuser'@'${db_address}' IDENTIFIED BY '${db_password}';\n" > temp.sql
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'${db_address}';\n" >> temp.sql
+echo "FLUSH PRIVILEGES;\n" >> temp.sql
+echo "Exit;\n" >> temp.sql
+
+mysql -p${db_password} -u root -h ${db_address} wordpress < temp.sql
+
+rm temp.sql
+
 cp wp-config-sample.php wp-config.php
 sed -i "/DB_NAME/c\define('DB_NAME', 'wordpress');'" wp-config.php
 sed -i "/DB_USER/c\define('DB_USER', 'wpuser');" wp-config.php
@@ -24,8 +30,8 @@ sed -i "/DB_HOST/c\define('DB_HOST', '${db_address}:${db_port}');" wp-config.php
 touch /etc/systemd/system/refreshLab.service
 touch /etc/systemd/system/refreshLab.timer
 touch /usr/local/bin/refreshLab.sh
-echo "${refreshLabService}" > /etc/systemd/system/refreshLab.service
-echo "${refreshLabTimer}" > /etc/systemd/system/refreshLab.timer
+echo '${refreshLabService}' > /etc/systemd/system/refreshLab.service
+echo '${refreshLabTimer}' > /etc/systemd/system/refreshLab.timer
 echo "${refreshLabScript}" > /usr/local/bin/refreshLab.sh
 
 systemctl start refreshLab.timer
