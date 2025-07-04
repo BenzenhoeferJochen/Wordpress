fbbc2c2fd0dc3b1656e5750ba01c7179abee2438
Jochen Benzenhöfer <jochen.benzenhoefer@it-tem.de>
2025-07-04 08:26:28 +0200

    enable Debug for wordpress

--- user_data.sh
+++ user_data.sh
@@ -12,8 +12,8 @@ chown -R apache:apache /var/www/html
 
 touch temp.sql
 
-echo "CREATE USER IF NOT EXISTS 'wpuser'@'${db_address}' IDENTIFIED BY '${db_password}';\n" > temp.sql
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'${db_address}';\n" >> temp.sql
+echo "CREATE USER IF NOT EXISTS 'wpuser'@'%' IDENTIFIED BY '${db_password}';\n" > temp.sql
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'%';\n" >> temp.sql
 echo "FLUSH PRIVILEGES;\n" >> temp.sql
 echo "Exit;\n" >> temp.sql
 
@@ -26,6 +26,8 @@ sed -i "/DB_NAME/c\define('DB_NAME', 'wordpress');'" wp-config.php
 sed -i "/DB_USER/c\define('DB_USER', 'wpuser');" wp-config.php
 sed -i "/DB_PASSWORD/c\define('DB_PASSWORD', '${db_password}');" wp-config.php
 sed -i "/DB_HOST/c\define('DB_HOST', '${db_address}:${db_port}');" wp-config.php
+sed -i "/WP_DEBUG/c\define('WP_DEBUG', 'true');" wp-config.php
+sed -i "/WP_DEBUG_LOG/c\define('WP_DEBUG_LOG', 'true');" wp-config.php
 
 touch /etc/systemd/system/refreshLab.service
 touch /etc/systemd/system/refreshLab.timer
@@ -35,7 +37,7 @@ echo '${refreshLabService}' > /etc/systemd/system/refreshLab.service
 echo '${refreshLabTimer}' > /etc/systemd/system/refreshLab.timer
 echo "${refreshLabScript}" > /usr/local/bin/refreshLab.sh
 
-chmod +x /usr/local/bin/refreshLab.sh
+chmod 744 /usr/local/bin/refreshLab.sh
 
 systemctl start refreshLab.timer
 systemctl enable refreshLab.timer
