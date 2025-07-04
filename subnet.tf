f81936e93fd976a50393fe924246a81463e8ecef
Jochen Benzenhöfer <jochen.benzenhoefer@it-tem.de>
2025-07-04 08:28:23 +0200

    private subnet

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
 
 # Public Subnet
-resource "aws_subnet" "Public_Subnet" {
+resource "aws_subnet" "Public_Subnet1" {
   vpc_id            = aws_vpc.Wordpress_VPC.id
   availability_zone = "${aws_vpc.Wordpress_VPC.region}a"
   cidr_block        = "10.0.0.0/28"
@@ -13,10 +13,25 @@ resource "aws_subnet" "Public_Subnet2" {
   cidr_block        = "10.0.0.16/28"
 }
 
+
+# Private Subnet 1
+resource "aws_subnet" "Private_Subnet1" {
+  vpc_id            = aws_vpc.Wordpress_VPC.id
+  availability_zone = "${aws_vpc.Wordpress_VPC.region}a"
+  cidr_block        = "10.0.0.32/28"
+}
+
+# Private Subnet 2
+resource "aws_subnet" "Private_Subnet2" {
+  vpc_id            = aws_vpc.Wordpress_VPC.id
+  availability_zone = "${aws_vpc.Wordpress_VPC.region}b"
+  cidr_block        = "10.0.0.48/28"
+}
+
 # Associate the Subnet to a route table
 resource "aws_route_table_association" "Public_Route_Table_Association" {
   route_table_id = aws_route_table.Public_Route_Table.id
-  subnet_id      = aws_subnet.Public_Subnet.id
+  subnet_id      = aws_subnet.Public_Subnet1.id
 }
 
 # Associate the Subnet to a route table
@@ -25,7 +40,19 @@ resource "aws_route_table_association" "Public_Route_Table_Association2" {
   subnet_id      = aws_subnet.Public_Subnet2.id
 }
 
+# Associate the Subnet to a route table
+resource "aws_route_table_association" "Private_Route_Table_Association" {
+  route_table_id = aws_route_table.Private_Route_Table.id
+  subnet_id      = aws_subnet.Private_Subnet1.id
+}
+
+# Associate the Subnet to a route table
+resource "aws_route_table_association" "Private_Route_Table_Association2" {
+  route_table_id = aws_route_table.Private_Route_Table.id
+  subnet_id      = aws_subnet.Private_Subnet2.id
+}
+
 resource "aws_db_subnet_group" "wordpress_db_subnet_group" {
   name       = "db_subnet_group"
-  subnet_ids = [aws_subnet.Public_Subnet.id, aws_subnet.Public_Subnet2.id]
+  subnet_ids = [aws_subnet.Private_Subnet1.id, aws_subnet.Private_Subnet2.id]
 }
--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,14 @@
 
 # Create a VPC
 resource "aws_vpc" "Wordpress_VPC" {
-  cidr_block = "10.0.0.0/27"
+  cidr_block = "10.0.0.0/26"
 }
 
 # Create a Route Table
 resource "aws_route_table" "Public_Route_Table" {
+  vpc_id = aws_vpc.Wordpress_VPC.id
+}
+
+resource "aws_route_table" "Private_Route_Table" {
   vpc_id = aws_vpc.Wordpress_VPC.id
 }
